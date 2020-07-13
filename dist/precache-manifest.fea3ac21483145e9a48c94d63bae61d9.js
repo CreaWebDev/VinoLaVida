3b9514d08a234eeb4456e6f7faddaa2d3c27fe3b
Pia Roel <33084683+CreaWebDev@users.noreply.github.com>
2020-07-13 21:28:14 +0200

    Add dist folder

---
+++ dist/precache-manifest.fea3ac21483145e9a48c94d63bae61d9.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7739545332627bbc6d",
+    "url": "/css/app.2193da13.css"
+  },
+  {
+    "revision": "739fdcd1ef13e03e857f1f2fd63409c0",
+    "url": "/footernav.json"
+  },
+  {
+    "revision": "dd7515d05079f2f24fc11cb1d9c35be8",
+    "url": "/img/Cab.png"
+  },
+  {
+    "revision": "d2dc7991f3848138651027fca495b032",
+    "url": "/img/Malbec.png"
+  },
+  {
+    "revision": "b65ba1b10ff25d0316793f67bf27ff29",
+    "url": "/img/PinotBlanc.png"
+  },
+  {
+    "revision": "a6dca162caa013cbda8bf84737d00c6f",
+    "url": "/img/PinotGris.png"
+  },
+  {
+    "revision": "1087f011a2a86b13a9156a751a814939",
+    "url": "/img/Pinotnoir.png"
+  },
+  {
+    "revision": "5bbf294c2732f04d5b9debd9134658e2",
+    "url": "/img/Riesling.png"
+  },
+  {
+    "revision": "b50031fb189ae6875667c27ebceb4364",
+    "url": "/img/SauvBlanc.png"
+  },
+  {
+    "revision": "e333737c7e1f30565f5e0375fa31ca6a",
+    "url": "/img/california.e333737c.png"
+  },
+  {
+    "revision": "e333737c7e1f30565f5e0375fa31ca6a",
+    "url": "/img/california.png"
+  },
+  {
+    "revision": "792c26b716f7bf940f3d7158b5d29a8e",
+    "url": "/img/carmenere.jpg"
+  },
+  {
+    "revision": "38beb846e45c5a03384c1cdae2e2bc9a",
+    "url": "/img/carmenere.png"
+  },
+  {
+    "revision": "4dce396b90135ad32d6bc420402cbb5a",
+    "url": "/img/chardonnay.png"
+  },
+  {
+    "revision": "82c145cc5692a880fd14dc4cc81b6be3",
+    "url": "/img/france.82c145cc.png"
+  },
+  {
+    "revision": "82c145cc5692a880fd14dc4cc81b6be3",
+    "url": "/img/france.png"
+  },
+  {
+    "revision": "86a00d323c79fd982dcc6fb896603a2a",
+    "url": "/img/germany.86a00d32.png"
+  },
+  {
+    "revision": "86a00d323c79fd982dcc6fb896603a2a",
+    "url": "/img/germany.png"
+  },
+  {
+    "revision": "6d5fa8991f55a280368c2de2067c73eb",
+    "url": "/img/grapes.png"
+  },
+  {
+    "revision": "fff8d7a4dd38a12c4a9552a5cc81a19e",
+    "url": "/img/item1_@3x.jpg"
+  },
+  {
+    "revision": "070bdd8a36129b0e5f5ff09cda474d47",
+    "url": "/img/item2_@3x.jpg"
+  },
+  {
+    "revision": "d93bceac795502fee80a99f2ea904208",
+    "url": "/img/item6_@3x.jpg"
+  },
+  {
+    "revision": "6d5fa8991f55a280368c2de2067c73eb",
+    "url": "/img/logo.6d5fa899.png"
+  },
+  {
+    "revision": "c17ef676354f75c5a488f39aa1e42217",
+    "url": "/img/pinotgris.jpg"
+  },
+  {
+    "revision": "25d0875fae4469334fa1d4c2d959aad3",
+    "url": "/img/pinotnoir.jpg"
+  },
+  {
+    "revision": "d29ca324368b6fa2ffd65e4dbda3526e",
+    "url": "/img/spain.d29ca324.png"
+  },
+  {
+    "revision": "d29ca324368b6fa2ffd65e4dbda3526e",
+    "url": "/img/spain.png"
+  },
+  {
+    "revision": "fbc7e87fe806f51c5b9cb542dd3d1056",
+    "url": "/img/topbanner-img.png"
+  },
+  {
+    "revision": "3c77a8d1c7be7976212a03a78957b3fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7739545332627bbc6d",
+    "url": "/js/app.defce042.js"
+  },
+  {
+    "revision": "f905e55167598bf97ea1",
+    "url": "/js/chunk-vendors.a6c3f9c7.js"
+  },
+  {
+    "revision": "78892ee4b7f63c5d4c0173864b3d1d4c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "89b2300547f235ebd299c358fe7ef485",
+    "url": "/products.json"
+  },
+  {
+    "revision": "2ead307d22e99fa607e97ddea4156e9a",
+    "url": "/tabs.json"
+  }
+]);
